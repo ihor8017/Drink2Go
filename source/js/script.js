30d8e84f0e8d6cae9e4e6df30eaf6861253e6c26
Ihor Chyzh <djar80@gmail.com>
2023-06-21 08:18:30 -0500

    добавляет стили к слайдеру

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,23 @@
-/* в этот файл добавляет скрипты*/
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+console.log("fucking JS");
